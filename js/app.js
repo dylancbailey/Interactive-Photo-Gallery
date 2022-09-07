ce3b06ea11a5f48f12bd5007adbc05b3d8bf8cec
Dylan Bailey <wearesleepless@gmail.com>
2022-09-07 09:08:16 -0400

    Adds custom search filter

--- js/app.js
+++ js/app.js
@@ -1,45 +1,51 @@
 // Initialize baguetteBox
 baguetteBox.run('.content__gallery');
 
-// Search feature
+// searchFilter.js
 // const search = new Filter('search', 'data-caption');
 
-// Search feature
+// // Custom search
 // function search_img() {
-//     let input = document.getElementById('search').value;
-//     input=input.toLowerCase();
-//     let x = document.getElementsByClassName('content__gallery__link');
+//     // Locate the img & search input
+//     let img = document.querySelectorAll('.content__gallery__link');
+//     let search_query = document.getElementById('search').value;
 
-//     for (let i = 0; i < x.length; i++) {
-//         if (!x[i].innerHTML.toLocaleLowerCase().includes(input)) {
-//             x[i].style.display="none";
-//         }
-//         else {
-//             x[i].style.display="initial";
+//     // Loop through the images
+//     for (let i = 0; i < img.length; i++) {
+//         // If the text is within the image
+//         if (img[i].getAttribute('data-caption').toLowerCase()
+//             // and the text matches the search
+//             .includes(search_query.toLowerCase())) {
+//                 // remove the 'is-hidden' class
+//                 img[i].classList.remove('is-hidden');  
+//                 console.log(img[i]); 
+//         } else {
+//             // Otherwise, add the class
+//             img[i].classList.add('is-hidden');
 //         }
 //     }
 // }
 
-// Search feature
-function search_img() {
-    // Locate the img
+// // Delay
+// let typingTimer;
+// let typeInterval = 5000;
+// let searchInput = document.getElementById('search');
+
+// searchInput.addEventListener('keyup', () => {
+//     clearTimeout(typingTimer);
+//     typingTimer = setTimeout(search_img, typeInterval);
+// });
+
+const img_search = document.getElementById('search');
+img_search.addEventListener('keyup', e => {
+    let currentValue = e.target.value.toLowerCase();
     let img = document.querySelectorAll('.content__gallery__link');
 
-    // Locate search input
-    let search_query = document.getElementById("search").value;
-
-    // Loop through the images
-    for (let i = 0; i < img.length; i++) {
-        // If the text is within the image
-        if (img[i].innerHTML.toLowerCase()
-            // and the text matches the search
-            .includes(search_query.toLowerCase())) {
-                // remove the 'is-hidden' class
-                img[i].classList.remove('is-hidden');  
-                console.log(img[i]); 
+    img.forEach(img_caption => {
+        if (img_caption.getAttribute('data-caption').toLowerCase().includes(currentValue)) {
+            img_caption.classList.remove('is-hidden');
         } else {
-            // Otherwise, add the class
-            img[i].classList.add('is-hidden');
+            img_caption.classList.add('is-hidden');
         }
-    }
-}
+    })
+});
