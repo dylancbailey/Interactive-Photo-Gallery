b7287a35830bad456fd19a5506cac22abcd5937d
Dylan Bailey <wearesleepless@gmail.com>
2022-09-07 12:19:14 -0400

    Adds a clear button

--- js/app.js
+++ js/app.js
@@ -4,11 +4,11 @@ baguetteBox.run('.content__gallery');
 // Custom search feature
 const img_search = document.getElementById('search');
 img_search.addEventListener('keyup', e => {
-    let currentValue = e.target.value.toLowerCase();
+    let current_value = e.target.value.toLowerCase();
     let img_caption = document.querySelectorAll('.content__gallery__link');
 
     img_caption.forEach(img => {
-        if (img.getAttribute('data-caption').toLowerCase().includes(currentValue)) {
+        if (img.getAttribute('data-caption').toLowerCase().includes(current_value)) {
             img.classList.remove('is-hidden');
         } else {
             img.classList.add('is-hidden');
@@ -16,6 +16,15 @@ img_search.addEventListener('keyup', e => {
     });
 });
 
+// Clear search
+let images = document.querySelectorAll('.content__gallery__link');
+
+function clearSearch() {
+    img_search.value = "";
+    images.forEach(img => {
+        img.classList.remove('is-hidden');
+    });
+}
 
 // Opacity animation
 const img_cards = document.querySelectorAll('.content__gallery__link');
