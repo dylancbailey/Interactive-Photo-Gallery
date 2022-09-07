bb730f468d53ee49e8754c73296cbe5aa3437779
Dylan Bailey <wearesleepless@gmail.com>
2022-09-07 09:52:59 -0400

    Cleans up unwanted code

--- js/app.js
+++ js/app.js
@@ -4,39 +4,7 @@ baguetteBox.run('.content__gallery');
 // searchFilter.js
 // const search = new Filter('search', 'data-caption');
 
-// // Custom search
-// function search_img() {
-//     // Locate the img & search input
-//     let img = document.querySelectorAll('.content__gallery__link');
-//     let search_query = document.getElementById('search').value;
-
-//     // Loop through the images
-//     for (let i = 0; i < img.length; i++) {
-//         // If the text is within the image
-//         if (img[i].getAttribute('data-caption').toLowerCase()
-//             // and the text matches the search
-//             .includes(search_query.toLowerCase())) {
-//                 // remove the 'is-hidden' class
-//                 img[i].classList.remove('is-hidden');  
-//                 console.log(img[i]); 
-//         } else {
-//             // Otherwise, add the class
-//             img[i].classList.add('is-hidden');
-//         }
-//     }
-// }
-
-// // Delay
-// let typingTimer;
-// let typeInterval = 5000;
-// let searchInput = document.getElementById('search');
-
-// searchInput.addEventListener('keyup', () => {
-//     clearTimeout(typingTimer);
-//     typingTimer = setTimeout(search_img, typeInterval);
-// });
-
-// Search feature
+// Custom search feature
 
 const img_search = document.getElementById('search');
 img_search.addEventListener('keyup', e => {
@@ -52,10 +20,11 @@ img_search.addEventListener('keyup', e => {
     })
 });
 
-// Opacity
+// Opacity animation
 
 const img_cards = document.querySelectorAll('.content__gallery__link');
 img_cards.forEach(img => {
     let randomDelay = Math.floor(Math.random() * 500);
     img.style.animation = `fadeIn 1s .${randomDelay}s ease forwards`;
 });
+
